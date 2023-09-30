87e96998eeaca0813403e6ed608b32e2262a65aa
tomi2102 <tomiolagunju@yahoo.com>
2023-09-30 14:23:22 +0100

    Finished Assignment

---
+++ even_number.py
@@ -0,0 +1,6 @@
+number=input("Please enter any number ")
+number=int(number)
+if number%2 == 0:
+    print("Your number is an even number.")
+else:
+    print("Your number is an odd number.")
---
+++ string_concatenation.py
@@ -0,0 +1,4 @@
+original = input("Enter any word to make a new word ")
+new=(original[0]+original[len(original)//2]+original[len(original)-1])
+print(new)
+
