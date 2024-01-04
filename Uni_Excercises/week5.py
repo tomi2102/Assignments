59ea8bae08f68a5cba8dd8881150c97b6cdfdfe0
tomi2102 <tomiolagunju@yahoo.com>
2024-01-04 16:47:38 +0000

    Remainder of Excercise 5

--- Uni_Excercises/week5.py
+++ Uni_Excercises/week5.py
@@ -17,5 +17,72 @@ print(roman)
 roman.pop(100000)
 print(roman)
 
+#Excercise1
+def display_dico(dico):
+    for key in dico:
+        print(key, '-->', dico[key])
+
+
+#Excercise2
+def concat_dico(dico1, dico2):
+    result = {}
+    for key, val in dico1.items():
+        result[key] = val
+
+    for key, val in dico2.items():
+        result[key] = val 
+
+    return result
+
+
+def concat_dico_advanced(dico1, dico2):
+    result = {}
+    for key, val in dico1.items():
+        result[key] = val
+
+    for key, val in dico2.items():
+        if key in result:
+            result[key] = [dico1[key],dico2[key]]
+
+        else:
+            result[key] = val
+
+    return result
+
+
+
+
+
+#Excercise3
+def map_list(keys, values):
+    if len(keys) != len(values):
+        return None
+    mapped = {}
+    for index in range(len(keys)):
+        if keys[index] not in mapped:
+            mapped[keys[index]] = values[index]
+        else:
+            print("Error: duplicate value", keys[index], "in the list of keys.")
+            return None
+
+    return mapped        
+
+
+
+
+
+
+
+#Excercise4
+def reverse_dictionary(dico):
+    reversed_dico = {}
+    for key, val in dico.items():
+        if val in reversed_dico:
+            print("Duplicate values in initial dictionary are not allowed.")
+            return None
+        else:
+            reversed_dico[val] = key
+
+    return reversed_dico
 
 
