cf0dfd74c17f0b50309e01591eae81d25f62c02e
tomi2102 <tomiolagunju@yahoo.com>
2024-01-18 11:17:15 +0000

    Object Question 2 Practise

--- object_practise2.py
+++ object_practise2.py
@@ -5,5 +5,19 @@ class  Employee:
         emp.salary = emp_salary
         emp.department = emp_department
 
-    def __eq__(emo, __value: object) -> bool:
-        pass
+    def assign_department(emp,emp_department):
+        emp.department = emp_department
+        emp.department.append(input("Change Department"))
+    
+    def print_employee_details(emp):
+        print(f"The employee {emp.name},ID number{emp.ID},is in {emp.department} and earns {emp.salary}")
+
+    def calculate_emp_salary(salary,hours_worked):
+        salary = input("What is the standard salary of this employee")
+        hours_worked = input("How many hours did they work")
+        if hours_worked > 50:
+            overtime = hours_worked - 50
+            overtime_amount = (overtime * (salary / 50))
+            salary = salary + overtime_amount
+
+    
